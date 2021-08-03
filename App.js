33874bce20dc0f99f66ef630202b6de8438a0762
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 14:05:38 +0300

    Rokas

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import {Provider} from 'react-redux';
 import {store} from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 
 export default function App() {
   return (
       <Provider store={store}>
-          <SafeAreaProvider>
-              <StatusBar style="auto" />
-              <HomeScreen />
-          </SafeAreaProvider>
+          <NavigationContainer>
+              <SafeAreaProvider>
+                  <StatusBar style="auto" />
+                  <HomeScreen />
+              </SafeAreaProvider>
+          </NavigationContainer>
       </Provider>
   );
 }
