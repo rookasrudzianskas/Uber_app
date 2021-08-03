3eeb062521d36d16425dd47ee9feaee46abcce61
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 14:10:14 +0300

    Rokas

--- navigation/router.js
+++ navigation/router.js
@@ -5,14 +5,19 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "../screens/HomeScreen";
+import MapScreen from "../screens/MapScreen";
 
 
 const Stack = createNativeStackNavigator();
 
 function Router() {
     return (
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName="HomeScreen">
                 <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+                    headerShown: false,
+                }}/>
+
+                <Stack.Screen name="MapScreen" component={MapScreen} options={{
                     headerShown: false,
                 }}/>
             </Stack.Navigator>
