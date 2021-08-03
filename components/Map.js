4498121772134b157106e6f1a0cb1dbd77ebb8d8
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 21:37:31 +0300

    Rokas

--- components/Map.js
+++ components/Map.js
@@ -7,6 +7,16 @@ const Map = () => {
     return (
         <View>
            <Text>I am the map</Text>
+            <MapView
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
+
+
         </View>
     );
 };
