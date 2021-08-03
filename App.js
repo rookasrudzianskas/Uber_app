3a9580b201f61d944a7381423458bc819de5f06c
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 11:14:30 +0300

    Rokas

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Uber app 🚀!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +13,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  }
 });
