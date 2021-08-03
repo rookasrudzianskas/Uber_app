6a116a85a9709275bdd9a5bb81978925c634db94
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 12:38:27 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from 'react-redux';
+import {store} from "./store";
 
 export default function App() {
   return (
-      <Provider>
+      <Provider store={store}>
         <View style={styles.container}>
           <StatusBar style="auto" />
           <Text>Uber app 🚀!</Text>
