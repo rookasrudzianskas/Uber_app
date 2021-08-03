31d1f2093c34475c54eef2662f0d5cdde8494545
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 13:07:24 +0300

    Rokas

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from "react-native";
+import {Image, SafeAreaView, Text, View} from "react-native";
 import styles from './styles';
+import tw from 'tailwind-react-native-classnames';
+
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Rokas</Text>
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image style={{width: 100, height: 100, resizeMode: 'contain'}} source={{
+                    uri: 'https://links.papareact.com/gzs'
+                }} />
             </View>
         </SafeAreaView>
     );
