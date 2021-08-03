8b3a963f09fa76ad364958844268a2f5cd930ac3
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 20:49:55 +0300

    Rokas

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -29,7 +29,7 @@ const HomeScreen = () => {
                         },
                     }}
                     onPress={(data, details = null) => {
-                        // console.log(data)
+                        console.log(data)
                         console.log(details)
                     }}
                     returnKeyType={"search"}
