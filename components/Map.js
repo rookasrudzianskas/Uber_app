a16b0c09c1f7d61aa657623d4b769d1218fc4631
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 21:37:53 +0300

    Rokas

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, View} from "react-native";
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 
 const Map = () => {
