81b5fe2caf927069ba4e4f011f0f9150ccaee8ff
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 21:44:59 +0300

    Rokas

--- components/Map.js
+++ components/Map.js
@@ -3,12 +3,13 @@ import {Dimensions, Text, View} from "react-native";
 import MapView, {Marker} from 'react-native-maps';
 import tw from "tailwind-react-native-classnames";
 import {StyleSheet} from "react-native";
+import tailwind from "tailwind-react-native-classnames";
 
 
 const Map = () => {
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map}
+        <View>
+            <MapView style={tw`flex-1`}
                 initialRegion={{
                     latitude: 37.78825,
                     longitude: -122.4324,
