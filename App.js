76c76953b0c381375f29ce37e8489fd8d75ebe9b
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 12:24:07 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
 
 export default function App() {
   return (
