3368444517038a891ddf67782ea5433a1f950156
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 13:21:37 +0300

    Rokas

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FlatList, Image, Text, TouchableOpacity, View} from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const data = [
     {
@@ -19,9 +20,10 @@ const data = [
 const NavOptions = () => {
     return (
         <FlatList data={data} horizontal keyExtractor={(item) => item.id} renderItem={({item}) => (
-            <TouchableOpacity>
+            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                 <View>
                     <Image style={{width: 120, height: 120, resizeMode: 'contain'}} source={{uri: item.image}} />
+                    <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
                 </View>
             </TouchableOpacity>
         )} />
