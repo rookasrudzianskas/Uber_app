fda2ae350a69f05b07ac55bc7be972b320ab2aab
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 14:13:32 +0300

    Rokas

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -25,7 +25,7 @@ const NavOptions = () => {
 
     return (
         <FlatList data={data} horizontal keyExtractor={(item) => item.id} renderItem={({item}) => (
-            <TouchableOpacity onPress={() => navigation.navigate('MapScreen')} activeOpacity={0.5} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            <TouchableOpacity onPress={() => navigation.navigate(item.screen)} activeOpacity={0.5} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                 <View>
                     <Image style={{width: 120, height: 120, resizeMode: 'contain'}} source={{uri: item.image}} />
                     <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
