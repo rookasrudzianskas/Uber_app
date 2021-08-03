cb0871bc456fe975d0ba4f05c261025a91e9a02c
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 21:36:50 +0300

    Rokas

---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native";
+import MapView from 'react-native-maps';
+
+
+const Map = () => {
+    return (
+        <View>
+           <Text>I am the map</Text>
+        </View>
+    );
+};
+
+export default Map;
--- screens/MapScreen/index.js
+++ screens/MapScreen/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Text, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
+import Map from "../../components/Map";
 
 const MapScreen = () => {
     return (
         <View>
-           <Text>Rokas</Text>
 
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
 
-            <View style={tw`h-1/2`}></View>
-            <View style={tw`h-1/2`}></View>
+            <View style={tw`h-1/2`}>
+
+            </View>
         </View>
     );
 };
