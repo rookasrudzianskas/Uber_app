5dc8b2913dffe137428240fe9f3c59df865cd259
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 21:33:21 +0300

    Rokas

--- screens/MapScreen/index.js
+++ screens/MapScreen/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const MapScreen = () => {
     return (
         <View>
            <Text>Rokas</Text>
+
+
+            <View style={tw`h-1/2`}></View>
+            <View style={tw`h-1/2`}></View>
         </View>
     );
 };
