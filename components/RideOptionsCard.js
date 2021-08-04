3042960515c68ce426fc617437b70ab2b50799df
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 12:17:38 +0300

    Rokas

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {SafeAreaView, Text, View} from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const RideOptionsCard = () => {
     return (
-        <View>
-            <Text>Rookas</Text>
-        </View>
+        <SafeAreaView>
+            <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        </SafeAreaView>
     );
 };
 
