369029861834a510b6eeb645c0dc09f88063903b
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 08:53:49 +0300

    Rokas

--- screens/MapScreen/index.js
+++ screens/MapScreen/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import {Text, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import Map from "../../components/Map";
+import {createStackNavigator} from "@react-navigation/stack";
 
 const MapScreen = () => {
+
+    const Stack = createStackNavigator();
+
     return (
         <View>
 
