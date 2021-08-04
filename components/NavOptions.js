9ba0943e25563e097fea26713aa62e3b4fbe54ca
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 08:49:37 +0300

    Rokas

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,8 @@ import {FlatList, Image, Text, TouchableOpacity, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import {Icon} from "react-native-elements";
 import {useNavigation} from "@react-navigation/native";
+import {useSelector} from "react-redux";
+import {selectOrigin} from "../slices/navSlice";
 
 const data = [
     {
@@ -21,12 +23,14 @@ const data = [
 
 const NavOptions = () => {
 
+
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
 
     return (
         <FlatList style={{}} data={data} horizontal keyExtractor={(item) => item.id} renderItem={({item}) => (
-            <TouchableOpacity onPress={() => navigation.navigate(item.screen)} activeOpacity={0.5} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                <View>
+            <TouchableOpacity disabled={!origin} onPress={() => navigation.navigate(item.screen)} activeOpacity={0.5} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+                <View style={tw`${!origin && 'opacity-20'}`}>
                     <Image style={{width: 120, height: 120, resizeMode: 'contain'}} source={{uri: item.image}} />
                     <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
 
