93e53afbd8003a193a63cc36a14ee317d66a8cdf
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 12:13:57 +0300

    Rokas

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -55,11 +55,17 @@ const NavigateCard = () => {
                 <NavFavourites />
             </View>
 
-            <View>
-                <TouchableOpacity>
+            <View style={tw`flex flex-row bg-white  justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                <TouchableOpacity style={tw`flex justify-between flex-row bg-black w-24 px-4 py-3 rounded-full`}>
                     <Icon name="car" type="font-awesome" color="white" size={16} />
                     <Text style={tw`text-white text-center`}>Rides</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16} />
+                    <Text style={tw` text-center`}>Eats</Text>
+                </TouchableOpacity>
+
             </View>
         </SafeAreaView>
     );
