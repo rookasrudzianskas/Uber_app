e7e0897413e2628b67e44d6df90ee820ed288b68
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 11:27:54 +0300

    Rokas

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -23,8 +23,13 @@ const NavFavourites = () => {
 
     return (
         <FlatList data={data} keyExtractor={item => item.id} renderItem={({item: {location, destination, icon }}) => (
-            <TouchableOpacity>
+            <TouchableOpacity style={tw`flex-row items-center p-5`}>
                 <Icon style={tw`mr-4 rounded-full bg-gray-300 p-3`} name={icon} type="ionicon" color="white" size={18} />
+
+                <View>
+                    <Text>{location}</Text>
+                    <Text>{destination}</Text>
+                </View>
             </TouchableOpacity>
         )}/>
     );
