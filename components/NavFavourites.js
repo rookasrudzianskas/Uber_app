2572d2ca740c66e6837f81cf741ca36fb6452da6
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 11:26:24 +0300

    Rokas

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {FlatList, Text, TouchableOpacity, View} from "react-native";
+import {Icon} from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
     const data = [
         {
@@ -20,9 +22,11 @@ const NavFavourites = () => {
 
 
     return (
-        <View>
-            <Text>Rokas</Text>
-        </View>
+        <FlatList data={data} keyExtractor={item => item.id} renderItem={({item: {location, destination, icon }}) => (
+            <TouchableOpacity>
+                <Icon style={tw`mr-4 rounded-full bg-gray-300 p-3`} name={icon} type="ionicon" color="white" size={18} />
+            </TouchableOpacity>
+        )}/>
     );
 };
 
