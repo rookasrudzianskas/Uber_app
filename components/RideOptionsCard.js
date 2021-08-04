bac639c0ea284c4d8b475aa10c015b70b843f3f0
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 12:23:31 +0300

    Rokas

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from "react-native";
+import {SafeAreaView, Text, TouchableOpacity, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
+import {Icon} from "react-native-elements";
+import {useNavigation} from "@react-navigation/native";
 
 const RideOptionsCard = () => {
+
+    const navigation = useNavigation();
     return (
         <SafeAreaView style={tw`bg-white flex-grow`}>
-            <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+            <View>
+
+                <TouchableOpacity onPress={() => navigation.navigate("NavigateCard")} style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+                    <Icon name="chevron-left" type="fontawesome" />
+                </TouchableOpacity>
+
+                <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+
+            </View>
         </SafeAreaView>
     );
 };
